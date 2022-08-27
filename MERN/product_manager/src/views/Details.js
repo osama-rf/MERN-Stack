78985658c054e48b04cd039a5d8c3860b66fb420
osama-rf <osama.refae12901@icloud.com>
2022-08-27 17:05:48 +0300

    1,2,3

--- MERN/product_manager/src/views/Details.js
+++ MERN/product_manager/src/views/Details.js
@@ -1,10 +1,11 @@
 import React, {useEffect, useState} from "react";
 import axios from "axios";
-import {Link, useParams} from "react-router-dom";
+import {Link, useParams, useNavigate} from "react-router-dom";
 
 const Details = (props) => {
     const [product, setProduct] = useState([])
     const {_id} = useParams();
+    const navigate = useNavigate()
 
     useEffect(() => {
         axios.get('http://localhost:8002/product/' + _id)
@@ -12,12 +13,26 @@ const Details = (props) => {
             .catch(err => console.error(err));
     }, []);
 
+    const {removeFromDom} = props;
+
+    const deleteProduct = (productId) => {
+        axios.delete('http://localhost:8002/product/' + productId)
+            .then(res => {
+                removeFromDom(productId);
+                navigate('/');
+            })
+            .catch(err => console.error(err))
+    }
+
     return (
         <div>
             <p>Product: {product.title}</p>
             <p>Price: {product.price}</p>
             <p>Description: {product.description}</p>
             <Link to={"/product/" + product._id + "/edit"}>Edit</Link>
+            <button onClick={(e) => {deleteProduct(product._id)}}>
+                Delete
+            </button>
         </div>
     )
 }
